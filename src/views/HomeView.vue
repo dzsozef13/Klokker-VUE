b66b9edac7c565bc160304bffc6c5ead850c7839
József Ádám Punk <punkjoad@gmail.com>
2023-04-16 14:51:41 +0200

    vue setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import TestComp from '../components/TestComp.vue'
+</script>
+
+<template>
+  <main>
+    <Suspense>
+      <template #default>
+        <TestComp/>
+      </template>
+      <template #fallback>
+        Nono
+      </template>
+    </Suspense>
+  </main>
+</template>
